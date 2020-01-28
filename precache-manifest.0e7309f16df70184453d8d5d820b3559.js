bae4681a52285d4612d6af9b75b54d89a954eb8b
Falshlight <teplinskij@inbox.ru>
2020-01-28 14:35:59 +0300

    Updates

---
+++ precache-manifest.0e7309f16df70184453d8d5d820b3559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69e57defe4a2ab953eaa523b073138a",
+    "url": "/goprotect-vk-mini-app/index.html"
+  },
+  {
+    "revision": "89d76f4c541ca941eb91",
+    "url": "/goprotect-vk-mini-app/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "89d76f4c541ca941eb91",
+    "url": "/goprotect-vk-mini-app/static/js/2.ef1c874e.chunk.js"
+  },
+  {
+    "revision": "25792abde4b14937c919",
+    "url": "/goprotect-vk-mini-app/static/js/main.c0e59efa.chunk.js"
+  },
+  {
+    "revision": "8473e9a2146183b4d4a3",
+    "url": "/goprotect-vk-mini-app/static/js/runtime-main.a8235852.js"
+  }
+]);
